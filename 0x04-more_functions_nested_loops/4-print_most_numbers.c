d5cb8612511fb8cdf42401eda90a63707945fda7
Wamuyu <m.wamuyu.kamau@gmail.com>
2022-10-23 11:05:33 -0700

    nested loop- print without

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - print the numbers 0 to 9
- * without 2 and 4
+ * print_most_numbers - print the numbers 0 to 9 without 2 and 4
  * Return: Always 0
  */
 void print_most_numbers(void)
