6c98053d90b0164847f914d25487a969589a0d4c
Wamuyu <m.wamuyu.kamau@gmail.com>
2022-10-25 11:41:19 -0700

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - updates integer value to 98
+ * @n: sets and updates the pointer
+ *
+ * Return: void means that it is correct
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
